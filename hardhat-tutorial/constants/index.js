3742ca5f7dcd8b33fd21303b491088e1ae0e4cd2
tobiWeb3 <106195537+tobiWeb3@users.noreply.github.com>
2022-06-11 21:42:39 +0200

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = "0xB58fcA29d4d2F52C0bda4d30C47337e20e0603ae";
 // URL for the Crypto Devs NFT Metadata
-const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+const METADATA_URL = "https://web3-nftcollection-k3kwmt1ja-tobiweb3.vercel.app/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
